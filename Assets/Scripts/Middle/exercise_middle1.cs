02c8b205bc2eb46afe9858e8642551daa50f2a16
Hdisa <ruslan.matveev03@mail.ru>
2022-08-10 15:15:52 +1000

    решение 2го среднего упражнения

---
+++ Assets/Scripts/Middle/exercise_middle1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exercise_middle1 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    /*int checkMaxNumber(string rowNumber)
+    {
+        
+    }*/
+}
